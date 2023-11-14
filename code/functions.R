f0830dbc231c3ff869585175bd2be6916e21c875
MonicaDartmouth <111899989+MonicaDartmouth@users.noreply.github.com>
2023-11-14 15:21:54 -0500

    Update functions.R
    added more documentation

--- code/functions.R
+++ code/functions.R
@@ -1,10 +1,11 @@
 
 # title:  Functions Library
-# author: Chris Leggett and Aislinn Gilmour
-# date:   2023-11-08
+# authors: Chris Leggett, Aislinn Gilmour, Monica Dimambro
+# date:   2023-11-14
 
-
-# plot data from one country at a time
+# Description: plot one variable at a time from one country at a time
+# Parameters: data frame of country data, country (row), variable called yvar (columns)
+# What the function returns: a dot plot of specified variable for specified country from df passed into function
 plot_one <- function(df=cleandata2, country, yvar) {
      df <- df %>%
           filter(Country %in%(country))
@@ -16,8 +17,9 @@ plot_one <- function(df=cleandata2, country, yvar) {
           theme_classic()
 }
 
-
-# save one country at a time for country-specific analysis
+# Description: split data for one country from main data frame for future country-specific analysis
+# Parameters: data frame of country data, where countries are represented in the rows
+# What the function returns: one RDAT file per country in the data set, containing a subset of the data for each country
 split_and_save_by_country <- function(data) {
   unique_countries <- unique(data$Country)
   
@@ -32,8 +34,18 @@ split_and_save_by_country <- function(data) {
   }
 }
 
-
-# SIR function
+# Description: define a function to pass into an ordinary differential equations function (ode()) as func=sir
+# example use: out_canada <- ode(y = init, times = times, func = sir, parms = parameters_canada)
+# Parameters: 
+# (1) intial proportion in each compartment (S, I, and R) 
+# (2) beta (the infection parameter) and gamma (the recovery parameter) which are related to R-naught in the beta/gamma = R-naught
+# (3) time from start of outbreak to final timepoint, in days and presented as string of integers by 1.
+# example parameters: 
+# set initial proportion in each compartment, typically Susceptible = 0.999999, Infected = 0.000001, Recovered = 0 as follows: 
+# init <- c(S = 1-1e-6, I = 1e-6, R = 0.0)
+# set parameters (beta = infection parameter, gamma = recovery parameter) as follows: parameters_canada <- c(beta = beta, gamma = gamma_canada)
+# set time frame (2 years = 730 days) with the following: times <- seq(0, 730, by = 1)
+# What the function returns: a list of the proportion of the population who is susceptible, infected, and recovered, to be used within the ode function
 sir <- function(time, state, parameters) {
      
      with(as.list(c(state, parameters)), {
